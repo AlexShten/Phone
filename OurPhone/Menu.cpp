f12a5db431ed7a603106640c094b6602c2b8f0e9
ALEX <ALEX@LENOVO>
2016-08-17 21:16:30 +0300

    +

--- OurPhone/Menu.cpp
+++ OurPhone/Menu.cpp
@@ -5,6 +5,15 @@ using namespace std;
 struct tm *LocalTime;
 time_t t;
 
+enum Keys
+{
+	up = 72,
+	down = 80,
+	left = 75,
+	right = 77,
+	enter = 13
+};
+
 enum ConsoleColor
 {
 	Black = 0,
@@ -51,13 +60,13 @@ int GetSecond()
 
 MENU::MENU()
 {
-	string _operator=" ";
-	int _internet=NULL;
-	int _scroll=NULL;
-	int _battery=NULL;
-	int _screen=NULL;
-	int _blackout=NULL;
-	int _off=NULL;
+	string _operator = " ";
+	int _internet = NULL;
+	int _scroll = NULL;
+	int _battery = NULL;
+	int _screen = NULL;
+	int _blackout = NULL;
+	int _off = NULL;
 }
 
 void MENU::ReadSettings()
@@ -113,7 +122,7 @@ void MENU::WriteSettings()
 		Fout << this->GetBattery() << endl;
 		Fout << this->GetScreen() << endl;
 		Fout << this->GetBlackout() << endl;
-		Fout << this->GetOff() << endl;		
+		Fout << this->GetOff() << endl;
 	}
 
 	Fout.close();
@@ -165,6 +174,7 @@ int MENU::GetBlackout()
 }
 void MENU::SetBlackout(int tmp)
 {
+	if (tmp < 1) tmp = 1;
 	_blackout = tmp;
 }
 int MENU::GetOff()
@@ -173,6 +183,7 @@ int MENU::GetOff()
 }
 void MENU::SetOff(int tmp)
 {
+	if (tmp < 1) tmp = 1;
 	_off = tmp;
 }
 
@@ -195,7 +206,7 @@ void MENU::PatternForPrint(int selection, int param, string _text1, string _text
 	cout << "\t";
 	if (selection == param) Set_Color(background1, text1);
 	else Set_Color(background2, text2);
-	if (printParam == 1) cout << param << ". " << _text1 <<"  "<< _text2 << endl;
+	if (printParam == 1) cout << param << ". " << _text1 << "  " << _text2 << endl;
 	if (printParam == 0) cout << _text1 << "  " << _text2;
 	Set_Color(background2, text2);
 }
@@ -239,7 +250,7 @@ void MENU::PrintMainScreen(int selection)
 void MENU::PrintMenu(int selection)
 {
 	system("cls");
-	cout << GetHour() << ":" << GetMinute() << ":" << GetSecond()<< endl;
+	cout << GetHour() << ":" << GetMinute() << ":" << GetSecond() << endl;
 	cout << "----------------------------------------------------------" << endl;
 	cout << "MENU..." << endl;
 	cout << "" << endl;
@@ -264,33 +275,36 @@ void MENU::PrintMenu(int selection)
 void MENU::PrintOptions(int selection)
 {
 	system("cls");
-	cout << GetHour() << ":" << GetMinute() << ":" << GetSecond()<< endl;
+	cout << GetHour() << ":" << GetMinute() << ":" << GetSecond() << endl;
 	cout << "----------------------------------------------------------" << endl;
 	cout << "OPTIONS..." << endl;
 	cout << "" << endl;
 
-	PatternForPrint(selection, 1, " Set operator: ", this->GetOperator(),1);
-	PatternForPrint(selection, 2, " ON/OFF internet: ",this->GetInternet()==1? "ON":"OFF", 1);
+	PatternForPrint(selection, 1, " Set operator: ", this->GetOperator(), 1);
+	PatternForPrint(selection, 2, " ON/OFF internet: ", this->GetInternet() == 1 ? "ON" : "OFF", 1);
 	PatternForPrint(selection, 3, " ON/OFF circular scrolling: ", this->GetScroll() == 1 ? "ON" : "OFF", 1);
 	PatternForPrint(selection, 4, " ON/OFF current battery charge level: ", this->GetBattery() == 1 ? "ON" : "OFF", 1);
 	PatternForPrint(selection, 5, " ON/OFF backlight off: ", this->GetScreen() == 1 ? "ON" : "OFF", 1);
 	PatternForPrint(selection, 6, " Set time before blackout, sec.: ", to_string(this->GetBlackout()), 1);
 	PatternForPrint(selection, 7, " Set time after blackout to off, sec.: ", to_string(this->GetOff()), 1);
+	PatternForPrint(selection, 8, " Set default settings ", "", 1);
 
-	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n";
 	cout << "----------------------------------------------------------" << endl;
 
 	cout << "\t\t\t\t";
-	PatternForPrint(selection, 8, "  BACK", "",0);
+	PatternForPrint(selection, 9, "  BACK", "", 0);
 	cout << "" << endl;
 }
+
 void MENU::MainMenu()
 {
 	int key;
 	int selection = 1;
 	int screen = 1;
 	int counter = 0;
-	int timer = 0;	
+	int timer = 0;
+	int editFlag = 0;
 
 	Organizer Org;
 	Calculator Calc;
@@ -307,11 +321,11 @@ void MENU::MainMenu()
 	{
 		Sleep(10);
 		counter++;
-		if(this->GetScreen() == 1) timer++;
+		if (this->GetScreen() == 1) timer++;
 
 		if (_kbhit())
 		{
-			key = _getch();		
+			key = _getch();
 
 			timer = 0;
 
@@ -326,23 +340,23 @@ void MENU::MainMenu()
 				if ((selection == 4 && this->GetScroll() == 0) || (selection == 0 && this->GetScroll() == 1)) selection = 3;
 				if ((selection == 4 && this->GetScroll() == 1) || (selection == 0 && this->GetScroll() == 0)) selection = 1;
 
-				if (key == 13)
+				if (key == enter)
 				{
 					switch (selection)
 					{
-					case 1:
+					case 1://MENU
 						screen = 2;
 						selection = 1;
 						this->PrintMenu(selection);
 						break;
 
-					case 2:
+					case 2://OPTIONS
 						screen = 3;
 						selection = 1;
 						this->PrintOptions(selection);
 						break;
 
-					case 3:
+					case 3://BACK
 						selection = 1;
 						this->PrintMainScreen(selection);
 					}
@@ -353,29 +367,29 @@ void MENU::MainMenu()
 				break;
 
 			case 2://Основное меню
-				if (key == 80) selection++;
-				if (key == 72) selection--;
+				if (key == down) selection++;
+				if (key == up) selection--;
 
 				if ((selection == 9 && this->GetScroll() == 0) || (selection == 0 && this->GetScroll() == 1)) selection = 8;
 				if ((selection == 9 && this->GetScroll() == 1) || (selection == 0 && this->GetScroll() == 0)) selection = 1;
 
-				if (key == 13)
+				if (key == enter)
 				{
 					switch (selection)
 					{
-					case 1:
+					case 1://Telephone book
 
 						break;
 
-					case 2:
+					case 2://SMS
 
 						break;
 
-					case 3:
+					case 3://Calls
 
 						break;
 
-					case 4:
+					case 4://Organizer
 						system("cls");
 						this->ScreenMode(75, 30);
 
@@ -386,7 +400,7 @@ void MENU::MainMenu()
 
 						break;
 
-					case 5:
+					case 5://Calculator
 						system("cls");
 						this->ScreenMode(75, 30);
 
@@ -396,11 +410,11 @@ void MENU::MainMenu()
 						this->PrintMenu(selection);
 						break;
 
-					case 6:
+					case 6://Games
 
 						break;
 
-					case 7:
+					case 7://Book store
 						system("cls");
 
 						Shop.ReadFile();
@@ -409,10 +423,11 @@ void MENU::MainMenu()
 						this->PrintMenu(selection);
 						break;
 
-					case 8:
+					case 8://BACK
 						screen = 1;
 						selection = 1;
 						this->PrintMainScreen(selection);
+						break;
 					}
 					break;
 				}
@@ -421,74 +436,118 @@ void MENU::MainMenu()
 				break;
 
 			case 3://Экран настроек
-				if (key == 80) selection++;
-				if (key == 72) selection--;
+				if (key == down && editFlag == 0)
+				{
+					selection++;
+					if (this->GetScreen() == 0 && selection == 6) selection += 2;//Пропуск настроек времени при неактивном пункте "Экран"
+				}
+				if (key == up && editFlag == 0)
+				{
+					selection--;
+					if (this->GetScreen() == 0 && selection == 7) selection -= 2;//Пропуск настроек времени при неактивном пункте "Экран"
+				}
 
-				if ((selection == 9 && this->GetScroll() == 0) || (selection == 0 && this->GetScroll() == 1)) selection = 8;
-				if ((selection == 9 && this->GetScroll() == 1) || (selection == 0 && this->GetScroll() == 0)) selection = 1;
+				if ((selection == 10 && this->GetScroll() == 0) || (selection == 0 && this->GetScroll() == 1)) selection = 9;//Реализация круговой прокрутки
+				if ((selection == 10 && this->GetScroll() == 1) || (selection == 0 && this->GetScroll() == 0)) selection = 1;
 
-				if (key == 13)
+				if (key == enter)
 				{
 					switch (selection)
 					{
-					case 1:
-						
+					case 1://Set operator
+
 						break;
 
-					case 2:
+					case 2://ON/OFF internet
 						this->GetInternet() == 0 ? this->SetInternet(1) : this->SetInternet(0);
 						this->WriteSettings();
 						break;
 
-					case 3:
+					case 3://ON/OFF circular scrolling
 						this->GetScroll() == 0 ? this->SetScroll(1) : this->SetScroll(0);
 						this->WriteSettings();
 						break;
 
-					case 4:
+					case 4://ON/OFF current battery charge level
 						this->GetBattery() == 0 ? this->SetBattery(1) : this->SetBattery(0);
 						this->WriteSettings();
 						break;
 
-					case 5:
+					case 5://ON/OFF backlight off
 						this->GetScreen() == 0 ? this->SetScreen(1) : this->SetScreen(0);
 						this->WriteSettings();
 						break;
 
-					case 6:
-
+					case 6://Set time before blackout
+						editFlag == 0 ? editFlag = 1 : editFlag = 0;
 						break;
 
-					case 7:
-
+					case 7://Set time after blackout to off
+						editFlag == 0 ? editFlag = 1 : editFlag = 0;
 						break;
 
-					case 8:
+					case 8://Set default settings
+						this->SetInternet(0);
+						this->SetScroll(0);
+						this->SetBattery(0);
+						this->SetScreen(1);
+						this->SetBlackout(5);
+						this->SetOff(5);
+						this->WriteSettings();
+						break;
+
+					case 9://BACK
 						screen = 1;
 						selection = 1;
 						this->PrintMainScreen(selection);
+						break;
 					}
 					break;
 				}
+
+				if (editFlag == 1)//Установа времени перед гашением и отключением экрана, в случае активации пункта "Экран"
+				{
+					switch (selection)
+					{
+					case 1:
+
+						break;
+
+					case 6:
+						if (key == down) SetBlackout(GetBlackout() - 1);
+						if (key == up) SetBlackout(GetBlackout() + 1);
+						this->WriteSettings();
+						break;
+
+					case 7:
+						if (key == down) SetOff(GetOff() - 1);
+						if (key == up) SetOff(GetOff() + 1);
+						this->WriteSettings();
+						break;
+					}
+				}
+				
+
 				this->PrintOptions(selection);
 				break;
+				
 			}
 		}
 
 
-		if (timer >= (this->GetBlackout() * 100 + this->GetOff() * 100) && this->GetScreen()==1) system("cls");//условие для отключения экрана при бездействвии
+		if (timer >= (this->GetBlackout() * 100 + this->GetOff() * 100) && this->GetScreen() == 1) system("cls");//условие для отключения экрана при бездействвии
 		else if (counter >= 100)
 		{
-			if (timer >= (this->GetBlackout() * 100) && timer < (this->GetBlackout() * 100 + this->GetOff()*100) && this->GetScreen() == 1)//Условие для затемнения шрифта при бездействии в течение времени
+			if (timer >= (this->GetBlackout() * 100) && timer < (this->GetBlackout() * 100 + this->GetOff() * 100) && this->GetScreen() == 1)//Условие для затемнения шрифта при бездействии в течение времени
 			{
 				background1 = Black; text1 = LightGray; background2 = DarkGray; text2 = Black;
 				Set_Color(background2, text2);
 			}
 
-			if (timer < (this->GetBlackout()*100))//Обновление экрана каждую секунду для функционирования часов
+			if (timer < (this->GetBlackout() * 100))//Обновление экрана каждую секунду для функционирования часов
 			{
-				background1 = Black; text1 = White; background2 = White; text2 = Black;
-				Set_Color(background2, text2);				
+				//background1 = Black; text1 = White; background2 = White; text2 = Black;
+				//Set_Color(background2, text2);
 			}
 
 			if (screen == 1) this->PrintMainScreen(selection);
@@ -496,7 +555,6 @@ void MENU::MainMenu()
 			if (screen == 3) this->PrintOptions(selection);
 			counter = 0;
 		}
-
-
+				
 	}
 }
